dd2e192ffd796445c4b5e685ee8bcf566dad9851
Silia Cronauer <silia.maria@web.de>
2022-10-25 15:38:23 +0200

    added js and portfolio

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+const projects = [{
+        name: "Foodblog",
+        img: "foodblog.jpg"
+    },
+    {
+        name: "Restaurant Website",
+        img: "restaurant.jpg"
+    },
+    {
+        name: "Angular Project",
+        img: "angular.jpg"
+    },
+    {
+        name: "Car Rental",
+        img: "car-rental.jpg"
+    }
+]
