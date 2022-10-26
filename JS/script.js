01d3b63e468080f8186b27503418ac1f018b5691
Silia Cronauer <silia.maria@web.de>
2022-10-26 16:16:39 +0200

    added scroll button, some media queeries

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,40 @@
+// Scrolling Btn - Progress Bar
+const progressBar = document.querySelector('.progressBar');
+const section = document.querySelector('section');
+
+const scrollProgressBar = () => {
+    let scrollDistance = -(section.getBoundingClientRect().top);
+    let progressPercentage =
+        (scrollDistance /
+            (section.getBoundingClientRect().height -
+                document.documentElement.clientHeight)) * 100;
+
+    let val = Math.floor(progressPercentage);
+    progressBar.style.width = val + '%';
+
+    if (val < 0) {
+        progressBar.style.width = '0%';
+    }
+};
+
+window.addEventListener('scroll', scrollProgressBar);
+
+// Hide/Show Button
+let toTopButton = document.querySelector(".scrollBtn");
+
+window.onscroll = function() { showButton() };
+
+function showButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTopButton.style.display = "block";
+    } else {
+        toTopButton.style.display = "none";
+    }
+}
+
+
+// Portfolio Loop
+
 const projects = [{
         name: "Foodblog",
         img: "foodblog.jpg",
